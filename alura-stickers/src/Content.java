011e26f12d38a5335894e2291ab4ba6f6f0c4172
Gilson Alencar <gilson.alencar94@gmail.com>
2023-04-03 14:20:57 -0300

    feat: add generic Content class

---
+++ alura-stickers/src/Content.java
@@ -0,0 +1,19 @@
+public class Content {
+
+  private final String title;
+  private final String imageURL;
+
+  public Content(String title, String imageURL) {
+    this.title = title;
+    this.imageURL = imageURL;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public String getImageURL() {
+    return imageURL;
+  }
+
+}
