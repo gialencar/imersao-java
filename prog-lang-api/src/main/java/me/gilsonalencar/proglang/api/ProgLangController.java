0cddb8bffe1984068429d7c447533b4c31d26cbf
Gilson Alencar <gilson.alencar94@gmail.com>
2023-04-05 15:33:56 -0300

    feat(progLangAPI): add PUT and DELETE endpoints for Language resource

--- prog-lang-api/src/main/java/me/gilsonalencar/proglang/api/ProgLangController.java
+++ prog-lang-api/src/main/java/me/gilsonalencar/proglang/api/ProgLangController.java
@@ -3,9 +3,14 @@ package me.gilsonalencar.proglang.api;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,9 +25,28 @@ public class ProgLangController {
   }
 
   @PostMapping(value = "/linguagens")
+  @ResponseStatus(code = HttpStatus.CREATED)
   public Language saveLanguage(@RequestBody Language language) {
     Language savedLanguage = repository.save(language);
     return savedLanguage;
   }
 
+  @PutMapping(value = "/linguagens")
+  public Language updateLanguage(@RequestBody Language language, @RequestParam String id) {
+
+    Language languageToUpdate = repository.findById(id).get();
+
+    languageToUpdate.setTitle(language.getTitle());
+    languageToUpdate.setImage(language.getImage());
+    languageToUpdate.setRanking(language.getRanking());
+
+    return repository.save(languageToUpdate);
+
+  }
+
+  @DeleteMapping(value = "/linguagens")
+  public ResponseStatus deleteLanguage(@RequestParam String id) {
+    repository.deleteById(id);
+    return null;
+  }
 }
